b3bbf8f6352a422bc419a8d9258badf3ae69ce14
dudujerry452 <dudujerry452@qq.com>
2025-05-23 15:19:36 +0800

    add renderer

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@
 #include "Game/Player.h"
 #include "Game/World.h"
 
+#include "Renderer/Renderer.h"
+
 #include "spdlog/spdlog.h"
 #include <iostream>
 
@@ -40,32 +42,8 @@ int main(void)
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
-
-    // Define the camera to look into our 3d world (position, target, up vector)
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };    // Camera position
-    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 60.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-
-    int cameraMode = CAMERA_FIRST_PERSON;
-
-    // Generates some random columns
-    float heights[MAX_COLUMNS] = { 0 };
-    Vector3 positions[MAX_COLUMNS] = { 0 };
-    Color colors[MAX_COLUMNS] = { 0 };
-
-    for (int i = 0; i < MAX_COLUMNS; i++)
-    {
-        heights[i] = (float)GetRandomValue(1, 12);
-        positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
-        colors[i] = (Color){ (unsigned char)GetRandomValue(20, 255), (unsigned char)GetRandomValue(10, 55), 30, 255 };
-    }
-
-    DisableCursor();                    // Limit cursor to relative movement inside the window
-
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    DisableCursor();                    
+    SetTargetFPS(60);                   
     //--------------------------------------------------------------------------------------
 
     LocalPlayer player;
@@ -74,68 +52,21 @@ int main(void)
 
     World world;
     auto plid = world.AddEntity(std::move(std::make_unique<LocalPlayer>(player)));
+    world.SetLocalPlayer(plid);
+
+    Renderer renderer;
+    renderer.Init();
+    renderer.Attach(&world);
 
     // Main game loop
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
         IM.CheckInput();
-
         world.WorldPhysicsUpdate();
-
-        camera.position = world.GetEntity(plid)->GetPos();
-        camera.target = world.GetEntity(plid)->GetPos() + world.GetEntity(plid)->GetForward();
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            BeginMode3D(camera);
-
-            for(auto obj : world.GetMap().objects) {
-                DrawCube(obj.colisionBoxes.min, 
-                obj.colisionBoxes.max.x, 
-                obj.colisionBoxes.max.y,
-                obj.colisionBoxes.max.z,
-                obj.color
-                );
-                DrawCubeWires(obj.colisionBoxes.min, 
-                obj.colisionBoxes.max.x, 
-                obj.colisionBoxes.max.y,
-                obj.colisionBoxes.max.z,
-                MAROON
-                );
-
-            }
-
-            EndMode3D();
-
-            // Draw info boxes
-
-            DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(600, 5, 195, 100, BLUE);
-
-            DrawText("Camera status:", 610, 15, 10, BLACK);
-            DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
-                                              (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
-                                              (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
-                                              (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
-            DrawText(TextFormat("- Projection: %s", (camera.projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
-                                                    (camera.projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
-            DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
-            Vector3 tmptar = camera.target;
-            DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", tmptar.x, tmptar.y, tmptar.z), 610, 75, 10, BLACK);
-            DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+        renderer.Render(&world);
     }
-
-    // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();        
 
     return 0;
 }
